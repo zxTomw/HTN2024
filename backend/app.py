290adf2b342d13aeaa939a25aed5d18e1e6879dc
zxTomw <tom_zhixing@outlook.com>
2024-09-14 20:25:30 -0400

    apis and theme

--- backend/app.py
+++ backend/app.py
@@ -6,17 +6,42 @@ import json
 load_dotenv()
 supabase = supabase_auth()
 
-airesp={
-    "msg":"htn"
-}
-airesp_string=json.dumps(airesp)
-# print(airesp_string)
+@app.route('/api/users', methods=['POST'])
+def create_user_handler():
+    user = request.json['user']
+    response = create_user(supabase, user)
+    return response
 
-voiceflow_response = response
+@app.route('/api/decks', methods=['GET'])
+def show_decks_handler():
+    user = request.json['user']
+    response = show_decks(supabase, user)
+    return json.dumps(response)
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return voiceflow_response.text
+@app.route('/api/users/<username>/decks/<deck_name>', methods=['GET'])
+def show_cards_in_deck_handler(username,deck_name):
+    user = username
+    deck_name = deck_name
+    response = show_cards_in_deck(supabase, user, deck_name)
+    return json.dumps(response)
+
+@app.route('/api/users/<username>/decks/<deck_name>', methods=['POST'])   
+def save_cards_in_deck_handler():
+    user = request.json['user']
+    deck_name = request.json['deck_name']
+    cards = request.json['cards']
+    response = save_cards_in_deck(supabase, user, deck_name, cards)
+    return json.dumps(response)
+
+@app.route('/api/users/<username>/decks', methods=['POST'])
+def add_deck_handler():
+    user = request.json['user']
+    deck_name = request.json['deck_name']
+    response = add_deck(supabase, user, deck_name)
+    return json.dumps(response)
+    
+
+
+
 
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
